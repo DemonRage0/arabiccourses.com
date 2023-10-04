f0556dfce6986dec083095128cc6abc529c9fa97
DemonRage0 <147001078+DemonRage0@users.noreply.github.com>
2023-10-04 22:08:47 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+var password = "arabe123"
+
+function passcheck() {
+    if(document.getElementById('arabe123').value !=password) {
+        alert('wrong password, try again');
+        return false;
+    }
+
+    if(document.getElementById('arabe123').value !=password) {
+        alert('correct password, click the button to proceed');
+}
+}
